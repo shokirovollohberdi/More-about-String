b015b04cd5272aea0cf3a81461e0a8f20165994f
Ollohberdi <85603736+shokirovollohberdi@users.noreply.github.com>
2022-07-06 13:05:47 +0000

    Initial commit

---
+++ main.dart
@@ -0,0 +1,40 @@
+void main() {
+
+// String a = "Hello World.";
+  // print(a.isEmpty);
+  // String a = "";
+  
+  // print(a.isNotEmpty);
+  // String a = "Hello World";
+  // print(a.length);
+
+  
+  // String a = "Hello World";
+  // print(a.contains("lo"));
+  
+  // String a = "Hello World";
+  // print(a.endsWith("ld"));
+  
+  // String a = "Hello World";
+  // print(a.startsWith("He"));
+  
+  // String a = "Hello World";
+  // print(a.indexOf("W"));
+  
+  // String a = "Hello World, I love programming";
+   //List<String> list = a.split("+");
+  //print(list);
+  
+  //String a = "   hello     ";
+    //print(a.trim());
+
+
+  //String word = "Flutter";
+  //print(word.substring(1, 2));
+
+  //String word = "yoo what's up bro";
+    //print(word.replaceAll("o","0"));
+
+
+  
+}
